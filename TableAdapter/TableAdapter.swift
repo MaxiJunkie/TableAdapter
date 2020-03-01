c59a4170adca7ff1b21c6f0300a9a078c9a97e27
MaxiAir <mstegnienko@agranovsky.org>
2020-03-02 00:39:33 +0300

    init

---
+++ TableAdapter/TableAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  TableAdapter.swift
+//  TableAdapter
+//
+//  Created by Максим Стегниенко on 02.03.2020.
+//  Copyright © 2020 MKSNS. All rights reserved.
+//
+
+import Foundation
+
+final class TableAdapter {
+    
+}
---
+++ TableAdapter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableAdapter
+//
+//  Created by Максим Стегниенко on 02.03.2020.
+//  Copyright © 2020 MKSNS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
